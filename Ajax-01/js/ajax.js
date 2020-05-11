e68ef1f06c8bf104e847082605ef9d7497f6d35a
DavidCasas <dcasas@webimpacto.es>
2020-05-11 17:25:26 +0200

    Trabajando con JSON

---
+++ Ajax-01/js/ajax.js
@@ -0,0 +1,34 @@
+(function(){
+
+    $.ajax({
+        type: 'GET',
+        url : 'http://www.json-generator.com/api/json/get/bIMlOkNSPS?indent=2',
+        dataType: 'json'
+    })
+    .done(function(data){
+        console.log("Hecho Correcto!");
+        var persona = data;
+        console.log(data);
+        $("#picFoto").attr("src",data.picture);
+        $("#txtNombre").val(data.name);
+        $("#txtDireccion").val(data.address);
+        $("#txtTelefono").val(data.phone);
+        $("#txtGenero").val(data.gender);
+
+
+
+    })
+    .fail(function(){
+        console.log("Fallo!");
+    })
+    .always(function(){
+        console.log("Completo!");
+    })
+
+
+
+
+
+
+
+})();
