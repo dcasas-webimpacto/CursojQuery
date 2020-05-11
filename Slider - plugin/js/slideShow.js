1fea223177b506971c6736956fc2d78f15a31276
DavidCasas <dcasas@webimpacto.es>
2020-05-11 10:43:14 +0200

    Slideshow a plugin-Parte2

--- Slider - plugin/js/slideShow.js
+++ Slider - plugin/js/slideShow.js
@@ -1,21 +1,61 @@
 (function(){
 
-	var actual = 0;
-	var ancho  = 600;
+	$.slideShow = function(opciones){
 
-	var $slideshow = $(".slideShow ul");
-	var slides  = $slideshow.find("li").length;
+	opciones = $.extend({
+		divDestino: ".slideShow",
+		intervalo: 1500,
+		ancho: 600,
+		velocidad: 1200,
+		color:"#ba007c",
+		slides: [],
+
+	}, opciones);
+
+	if(opciones.slides.length==0){
+		alert("Los slides son necesarios.");
+		return;
+	}
+	opciones.velocidad = opciones.velocidad/1000;
+	var actual = 0;
+	var ancho  = opciones.ancho;
+
+	
+	var slides  = opciones.slides.length;
+
+	//CREACION DEL SLIDESHOW
+	var contenido ="";
+
+		contenido+="<ul>"
+		for( var i=0; i< opciones.slides.length; i++){
+		contenido +='<li><img src="'+opciones.slides[i]+'"></li>';
+		}
+		contenido+="</ul>";
+		console.log(contenido);
+		$(opciones.divDestino).append(contenido);
+		var $slideshow = $(opciones.divDestino + "ul");
+
+	//CREACION DE LOS BOTONES
+		contenido="";
+		conenido += '<div class="slideShowButtons">';
+		for(var i=0; i<opciones.slides.length;i++){
+			contenido += '<div data-idx="'+i+'" class="slideButton" ></div>'
+		}
+		contenido += "</div>";
+		$(opciones.divDestino).append(contenido);
+
+
 	var $puntos = $(".slideShowButtons");
 
 	$puntos.find("div").eq(0).css({
-		backgroundColor:"#58167d"
+		backgroundColor:opciones.color
 	});
 
 	var intervalo = setInterval(function(){
 
 		mover("sig");
 
-	}, 1500 );
+	}, opciones.intervalo );
 
 
 	function mover( dir, click ){
@@ -48,8 +88,8 @@
 
 
 		var tl = new TimelineMax();
-		tl.to( $slideshow, 1.2, { marginLeft: margen, ease: Elastic.easeOut.config(1, 0.75) } )
-			.to( $puntoActual,0.5, {backgroundColor:"#58167d"}, "-=1.2")
+		tl.to( $slideshow, opciones.velocidad, { marginLeft: margen, ease: Elastic.easeOut.config(1, 0.75) } )
+			.to( $puntoActual,0.5, {backgroundColor:opciones.color}, "-=1.2")
 			.to( $demasPuntos,0.5, {backgroundColor:"#a1a1a1"}, "-=1.2");
 
 		// $slideshow.animate({
@@ -76,5 +116,16 @@
 
 	});
 
+
+
+
+
+
+	};
+
+
+
+
+	
 
 })();
